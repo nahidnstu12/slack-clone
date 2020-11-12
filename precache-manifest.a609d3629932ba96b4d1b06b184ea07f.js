d7619fa1cad39e741e76e04ab652472196d2bc93
= <=>
2020-11-12 19:13:44 +0600

    Updates

---
+++ precache-manifest.a609d3629932ba96b4d1b06b184ea07f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "52ae22f849a925c55be2e74d39f9a1a0",
+    "url": "/slack-clone/index.html"
+  },
+  {
+    "revision": "eb502b60f039bbc38887",
+    "url": "/slack-clone/static/css/main.c1a6adc7.chunk.css"
+  },
+  {
+    "revision": "770af447f56960c6efed",
+    "url": "/slack-clone/static/js/2.2d28328c.chunk.js"
+  },
+  {
+    "revision": "b3c9c9a6ee4657c08d9e688faf15105b",
+    "url": "/slack-clone/static/js/2.2d28328c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eb502b60f039bbc38887",
+    "url": "/slack-clone/static/js/main.ab7c52f6.chunk.js"
+  },
+  {
+    "revision": "f6fc9e51b945821decc8",
+    "url": "/slack-clone/static/js/runtime-main.48a8fb26.js"
+  }
+]);
